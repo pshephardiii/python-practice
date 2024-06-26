39a015fec0ccdaadbd862c5350f1cf64976357b8
Paul Shephard <paulshephard598@gmail.com>
2024-06-26 16:21:19 -0400

    added notes, some f-string examples, and a coding exercise (print sum of squares of first 10 numbers)

---
+++ practice.py
@@ -0,0 +1,17 @@
+# f-strings: examples
+
+# for i in range(1, 11):
+#     print(f"9 / {i} = {9 / i}")
+
+# a = 20
+
+# for i in range(1, 9):
+#     print(f"The sum of {a} and {i} is {a + i}")
+
+# Coding Exercise: Print Sum of Squares Of First 10 Numbers
+
+# sum_of_squares = 0
+# for i in range(1, 11):
+#     sum_of_squares += (i * i)
+    
+# print(sum_of_squares)
