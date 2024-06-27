ce4314d8083defd56ac1a0ae64c44bd0f1522439
Paul Shephard <paulshephard598@gmail.com>
2024-06-26 20:54:37 -0400

    added exercise: check if perfect number

--- practice.py
+++ practice.py
@@ -138,3 +138,19 @@
 #         if n % i == 0:
 #             sum += i
 #     return sum
+
+# Coding Exercise: check if perfect number
+
+# def is_perfect_number(n):
+#     if n <= 0:
+#         return False
+#     sum = 0
+    
+#     for i in range(1, n):
+#         if n % i == 0:
+#             sum += i
+    
+#     if sum == n:
+#         return True
+#     else:
+#         return False
