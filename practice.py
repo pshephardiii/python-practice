2c12ecef98c5f7e1a4c62d648ec8304b2a72a7d3
Paul Shephard <paulshephard598@gmail.com>
2024-06-27 10:56:34 -0400

    added exercise: is_prime

--- practice.py
+++ practice.py
@@ -213,3 +213,17 @@
 #         return "It's cool! A light jacket will do."
 #     else:
 #         return "It's warm! Enjoy the day."
+
+# For Loop vs. While Loop Exercises
+
+# Practice Exercise: is prime
+
+# def is_prime(n):
+#     if n < 2:
+#         return False
+        
+#     for i in range(2, n - 1):
+#         if n % i == 0:
+#             return False
+    
+#     return True
