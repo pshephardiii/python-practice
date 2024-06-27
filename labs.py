a88746ba5170a4990db98e757ad69adb408771f5
Paul Shephard <paulshephard598@gmail.com>
2024-06-27 16:03:02 -0400

    Lab 2 complete

--- labs.py
+++ labs.py
@@ -10,3 +10,25 @@
 #         if inches < 0:
 #             self.feet = -1
 #             self.inches = -1
+
+# Lab 2 - square class
+
+# class Square:
+#     def __init__(self, side): 
+#         if side <= 0:
+#             self.side = -1
+#         else:
+#             self.side = side
+
+#     def calculate_area(self):
+#         if self.side == -1:
+#             self.area = -1
+#         else: 
+#             self.area = self.side ** 2
+#         return self.area
+#     def calculate_perimeter(self):
+#         if self.side == -1:
+#             self.perimeter = -1
+#         else:
+#             self.perimeter = self.side * 4
+#         return self.perimeter
