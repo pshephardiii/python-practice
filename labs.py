1ccbe4c72a4cd8054b4af3922712f487abfa16e5
Paul Shephard <paulshephard598@gmail.com>
2024-06-27 15:33:15 -0400

    added lab: Lab 1

---
+++ labs.py
@@ -0,0 +1,12 @@
+# Object Classes and Constructors
+
+# Lab 1 - inches should be converted to feet and inches.
+
+# class Dimension:
+
+#     def __init__(self, inches): 
+#         self.feet = inches // 12
+#         self.inches = inches % 12
+#         if inches < 0:
+#             self.feet = -1
+#             self.inches = -1
