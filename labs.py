2b50d8aed10db26710dc3cea5cfc540a5120a0de
Paul Shephard <paulshephard598@gmail.com>
2024-06-28 16:27:42 -0400

    Lab 13: abstract class

--- labs.py
+++ labs.py
@@ -226,3 +226,40 @@
 # class Car(Engine, Wheels): 
 #     def drive(self):
 #         return "Car is driving"
+
+# Lab 13: Abstract Class
+
+# from abc import ABC, abstractmethod
+
+# class Shape(ABC):
+    
+#     @abstractmethod
+#     def area(self):
+#         pass
+
+#     @abstractmethod
+#     def perimeter(self):
+#         pass
+
+# # Rectangle class inheriting from Shape
+# class Rectangle(Shape):
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
+
+#     def area(self):
+#         return self.length * self.width
+
+#     def perimeter(self):
+#         return (self.length * 2) + (self.width * 2)
+
+# # Circle class inheriting from Shape
+# class Circle(Shape):
+#     def __init__(self, radius):
+#         self.radius = radius
+
+#     def area(self):
+#         return 3.14 * (self.radius ** 2)
+
+#     def perimeter(self):
+#         return 2 * 3.14 * self.radius
