053a6b4cdcef423c8d9ff57ea3068a5598ad09e2
Paul Shephard <paulshephard598@gmail.com>
2024-06-30 15:50:21 -0400

    Lab 22: distance between two points

--- labs.py
+++ labs.py
@@ -372,3 +372,13 @@
 #     name, age, grade = student
     
 #     return f"Student Name: {name}, Age: {age}, Grade: {grade}"
+
+# Lab 22: Distance between two points
+
+# import math
+
+# def calculate_distance(point1, point2):
+#     x1, y1 = point1
+#     x2, y2 = point2
+    
+#     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
