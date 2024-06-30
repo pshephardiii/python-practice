9125530cee9aad4402046dabc7860fc6c86077cd
Paul Shephard <paulshephard598@gmail.com>
2024-06-30 14:31:34 -0400

    Lab 15: count characters

--- labs.py
+++ labs.py
@@ -281,5 +281,16 @@
         
 #     return numbers
 
+# Lab 15: Count Characters 
+
+# def count_characters(str):
+#     char_count = {}
+    
+#     for char in str:
+#         if char in char_count:
+#             char_count[char] += 1
+#         else:
+#             char_count[char] = 1
+#     return char_count
 
 
